fe797bb91c012d613968b9548c3d55585b0b1298
metw0 <metwojustfunny@duck.com>
2026-05-31 01:20:41 +0300

    refactor: deleted cludge in redactor.ts

--- src/redactor.ts
+++ src/redactor.ts
@@ -30,11 +30,7 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'error',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-misused-promises': 'error',
-      '@typescript-eslint/explicit-function-return-type': (node) => {
-        if (node.type === 'FunctionDeclaration') {
-          return true;
-        }
-      },
+      '@typescript-eslint/explicit-function-return-type': ['error', { allowedNames: [] }],
       'no-console': 'warn',
       'curly': 'error',
       'import/order': ['warn', { alphabetize: { order: 'asc' } }],
@@ -47,41 +43,29 @@ export default tseslint.config(
 );`
 
     fs.writeFileSync(eslintPath, configContent)
-    console.log('eslint.config.mjs has been created')
+    console.log('eslint.config.mjs created')
   } catch (err) {
-    console.error('error while writing eslint.config.mjs: ', err)
+    console.error('error writing eslint config:', err)
   }
 }
 
-export function modifyPrittierCfg() {
+export function configurePrettier() {
   const prettierrcPath = path.join(process.cwd(), '.prettierrc')
 
-  if (fs.existsSync(prettierrcPath)) {
-    try {
-      let prettierConfig = JSON.parse(fs.readFileSync(prettierrcPath, 'utf8'))
-      prettierConfig = {
-        arrowParens: 'always',
-        bracketSameLine: false,
-        bracketSpacing: true,
-        embeddedLanguageFormatting: 'auto',
-        endOfLine: 'lf',
-        jsxSingleQuote: false,
-        printWidth: 100,
-        proseWrap: 'always',
-        quoteProps: 'as-needed',
-        semi: false,
-        singleQuote: true,
-        tabWidth: 2,
-        trailingComma: 'all',
-        useTabs: false,
-      }
-      fs.writeFileSync(prettierrcPath, JSON.stringify(prettierConfig, null, 2))
-      console.log('.prettierrc has been updated')
-    } catch (err) {
-      console.error('error while reading or writing .prettierrc: ', err)
-    }
-  } else {
-    console.log('.prettierrc does not exist in the current directory')
+  const prettierConfig = {
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'all',
+    printWidth: 100,
+    endOfLine: 'lf'
+  }
+
+  try {
+    fs.writeFileSync(prettierrcPath, JSON.stringify(prettierConfig, null, 2))
+    console.log('.prettierrc updated')
+  } catch (err) {
+    console.error('error writing prettier config:', err)
   }
 }
 
@@ -90,31 +74,23 @@ export function modifyTSCfg() {
 
   if (fs.existsSync(tsconfigPath)) {
     try {
-      const tsconfigConfig = JSON.parse(fs.readFileSync(tsconfigPath, 'utf8'))
-      tsconfigConfig.compilerOptions = {
+      const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, 'utf8'))
+      
+      tsconfig.compilerOptions = {
+        ...tsconfig.compilerOptions,
         module: 'nodenext',
         target: 'esnext',
-        types: ['node'],
-        sourceMap: true,
-        declaration: true,
-        declarationMap: true,
         strict: true,
-        noImplicitAny: true,
-        noUnusedLocals: true,
-        noUnusedParameters: true,
-        noFallthroughCasesInSwitch: true,
-        esModuleInterop: true,
         outDir: './dist',
         rootDir: './src',
       }
-      tsconfigConfig.include = ['src/**/*']
-      fs.writeFileSync(tsconfigPath, JSON.stringify(tsconfigConfig, null, 2))
-      console.log('tsconfig.json has been updated')
+      tsconfig.include = ['src/**/*']
+
+      fs.writeFileSync(tsconfigPath, JSON.stringify(tsconfig, null, 2))
+      console.log('tsconfig.json updated')
     } catch (err) {
-      console.error('error while reading or writing tsconfig.json: ', err)
+      console.error('error modifying tsconfig:', err)
     }
-  } else {
-    console.log('tsconfig.json does not exist in the current directory')
   }
 }
 
@@ -123,20 +99,20 @@ export function addScripts() {
 
   if (fs.existsSync(packagePath)) {
     try {
-      const packageConfig = JSON.parse(fs.readFileSync(packagePath, 'utf8'))
-      packageConfig.scripts = {
+      const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'))
+      
+      pkg.scripts = {
+        ...pkg.scripts,
         start: 'tsx src/index.mts',
-        build: 'rm -rf dist && mkdir dist && tsc',
-        test: 'tsc --noEmit --pretty',
-        lint: 'eslint "src/**/*.ts" --fix',
-        format: 'prettier --write "src/**/*.ts"',
+        build: 'rm -rf dist && tsc',
+        lint: 'eslint "src/**/*.{ts,mts}" --fix',
+        format: 'prettier --write "src/**/*.{ts,mts}"',
       }
-      fs.writeFileSync(packagePath, JSON.stringify(packageConfig, null, 2))
-      console.log('package.json has been updated')
+
+      fs.writeFileSync(packagePath, JSON.stringify(pkg, null, 2))
+      console.log('package.json scripts updated')
     } catch (err) {
-      console.error('error while reading or writing package.json: ', err)
+      console.error('error updating package.json:', err)
     }
-  } else {
-    console.log('package.json does not exist in the current directory')
   }
 }
